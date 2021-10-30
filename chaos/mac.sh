acaba932bc9ff0c4542e59dd7f4e3c58a1efe746
diartyz <diartyz@gmail.com>
2021-10-30 21:33:02 +0800

    fix(layzgit): lazygit folder doesn't exist

--- chaos/mac.sh
+++ chaos/mac.sh
@@ -8,3 +8,9 @@ defaults write com.apple.dock persistent-apps -array; killall Dock
 
 # enable PressAndHold
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# music language
+defaults write /Users/diartyz/Library/Preferences/com.apple.Music.plist AppleLanguages -array zh_CN
+
+# allow apps from anywhere
+sudo spctl --master-disable
