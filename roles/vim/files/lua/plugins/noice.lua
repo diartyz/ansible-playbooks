8818ed109d6c2304a1cdf9fd17feacc2112f5464
diartyz <diartyz@gmail.com>
2023-05-06 18:05:49 +0800

    refactor(vim): disable noice

--- roles/vim/files/lua/plugins/noice.lua
+++ roles/vim/files/lua/plugins/noice.lua
@@ -14,6 +14,12 @@ return {
   },
   config = function()
     require('noice').setup {
+      cmdline = {
+        enabled = false,
+      },
+      messages = {
+        enabled = false,
+      },
       lsp = {
         progress = {
           enabled = false,
