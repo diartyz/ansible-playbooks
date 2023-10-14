d94c6350d79f54af028354eb35cea4af7eef2d34
diartyz <diartyz@gmail.com>
2023-10-14 21:40:52 +0800

    refactor: ubuntu
    Signed-off-by: diartyz <diartyz@gmail.com>

--- roles/vim/files/lua/plugins/bufferline.lua
+++ roles/vim/files/lua/plugins/bufferline.lua
@@ -6,6 +6,8 @@ return {
     require('bufferline').setup {
       options = {
         indicator = { style = 'underline' },
+        max_name_length = 99,
+        max_prefix_length = 99,
         show_buffer_close_icons = false,
         show_buffer_icons = false,
         tab_size = 0,
--- roles/vim/files/lua/plugins/comment.lua
+++ roles/vim/files/lua/plugins/comment.lua
@@ -4,6 +4,7 @@ return {
     'JoosepAlviste/nvim-ts-context-commentstring',
     dependencies = 'nvim-treesitter/nvim-treesitter',
   },
+  keys = { 'gcc', { 'gc', mode = 'x' } },
   config = function()
     require('Comment').setup {
       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
