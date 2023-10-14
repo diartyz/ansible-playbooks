c13baf51d7a617c38515cefc2e7f800d4b32f2c9
diartyz <diartyz@gmail.com>
2023-10-14 21:40:52 +0800

    refactor: ubuntu
    Signed-off-by: diartyz <diartyz@gmail.com>

--- roles/vim/files/lua/plugins/bufferline.lua
+++ roles/vim/files/lua/plugins/bufferline.lua
@@ -5,10 +5,14 @@ return {
   config = function()
     require('bufferline').setup {
       options = {
+        style_preset = require('bufferline').style_preset.no_italic,
+        color_icons = false,
         indicator = { style = 'underline' },
-        show_buffer_close_icons = false,
-        show_buffer_icons = false,
+        max_name_length = 99,
+        max_prefix_length = 99,
+        sort_by = 'directory',
         tab_size = 0,
+        truncate_names = false,
       },
     }
 
@@ -21,6 +25,23 @@ return {
     vim.keymap.set('n', '<leader>7', '<cmd>BufferLineGoToBuffer 7<cr>')
     vim.keymap.set('n', '<leader>8', '<cmd>BufferLineGoToBuffer 8<cr>')
     vim.keymap.set('n', '<leader>9', '<cmd>BufferLineGoToBuffer 9<cr>')
-    vim.keymap.set('n', '<leader>0', '<cmd>BufferLineGoToBuffer 10<cr>')
+    vim.keymap.set('n', '<leader>0', '<cmd>BufferLineGoToBuffer 0<cr>')
+    vim.keymap.set('n', '<leader>X', '<cmd>BufferLinePickClose<cr>')
+    vim.keymap.set('n', '<leader>d', '<cmd>BufferLineCloseOthers<cr>')
+    vim.keymap.set('n', '<leader>t', '<cmd>BufferLinePick<cr>')
+    vim.keymap.set('n', '[B', function() require('bufferline').go_to(1, true) end)
+    vim.keymap.set('n', ']B', function() require('bufferline').go_to(0, true) end)
+    vim.keymap.set('n', '[b', function()
+      local count = vim.v.count > 0 and vim.v.count or 1
+      for _ = 1, count do
+        vim.cmd 'BufferLineCyclePrev'
+      end
+    end)
+    vim.keymap.set('n', ']b', function()
+      local count = vim.v.count > 0 and vim.v.count or 1
+      for _ = 1, count do
+        vim.cmd 'BufferLineCycleNext'
+      end
+    end)
   end,
 }
