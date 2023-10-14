a5ede78cace9417ed56c90fe9498743af165665f
diartyz <diartyz@gmail.com>
2023-10-14 21:40:52 +0800

    refactor: ubuntu
    Signed-off-by: diartyz <diartyz@gmail.com>

--- roles/vim/files/lua/plugins/toggleterm.lua
+++ roles/vim/files/lua/plugins/toggleterm.lua
@@ -3,18 +3,17 @@ return {
   keys = {
     [[<c-\>]],
     {
-      '-',
+      '+',
       function() require('toggleterm.terminal').Terminal:new({ cmd = 'ranger', count = 8 }):toggle() end,
     },
     {
-      '<c-l>',
+      '-',
       function() require('toggleterm.terminal').Terminal:new({ cmd = 'lazygit', count = 9 }):toggle() end,
     },
   },
   opts = {
     direction = 'float',
     float_opts = {
-      border = 'none',
       width = function() return vim.o.columns end,
       height = function() return vim.o.lines end,
     },
